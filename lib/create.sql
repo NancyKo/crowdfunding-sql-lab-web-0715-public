9ff11afc551e3dd01c0da00f88a4384af0beab1f
N K <konancys@gmail.com>
2015-08-15 10:50:05 -0400

    Created tables

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,22 @@
 -- your code here
+CREATE TABLE projects(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT,
+	category TEXT,
+	funding_goal INTEGER,
+	start_date DATE,
+	end_date DATE
+);
+
+CREATE TABLE users(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT,
+	age INTEGER
+);
+
+CREATE TABLE pledges(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	amount INTEGER,
+	user_id INTEGER REFERENCES users,
+	project_id INTEGER REFERENCES projects
+);
